0d88a301e1ca0a7c0eb900d06355bbb52f646403
Carlos Carrasquillo <c.carrasquillo@ufl.edu>
2020-01-22 09:38:33 -0500

    bug fixes, spelling corrections

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,8 +7,8 @@ class Footer extends React.Component {
         return(
           <div className="footer">
               <p>
-                  So normally, this is the place where people add a subsription form or a second navbar or something. I would just like to shoutout Stack Overflow and the many
-                  tutorials out there that helped me make this possible! More about that in the <a href="/../HowItsMade#sources">How It's Made</a> section. 
+                  So normally, this is the place where people add a subscription form or a second navbar or something. I would just like to shoutout Stack Overflow and the many
+                  tutorials out there that helped me make this possible! More about that in the <a href="/../HowItsMade#sources">How It's Made</a> section.
               </p>
           </div>
         )
